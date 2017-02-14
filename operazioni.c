181eee7e9e738b89de911c68c0cded7f23484c72
LeonardoRiselli <juniorriselli@gmail.com>
2017-02-14 17:13:19 +0100

    Operazioni in C

---
+++ operazioni.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+
+  int n1, n2, ris;
+
+  printf ( "dimmi il primo numero :");
+  scanf ("%d", &n1);
+  printf ( "dimmi il secondo numero :");
+  scanf ("%d", &n2);
+
+  ris=n1+n2;
+
+  printf ("il risultato è %d \n :", ris);
+
+  return 0;
+}
