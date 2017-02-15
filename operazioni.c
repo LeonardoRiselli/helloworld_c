957d81aaf993ca1ae027173d5145696b1df256ee
LeonardoRiselli <juniorriselli@gmail.com>
2017-02-15 13:56:57 +0100

    Operazioni aggiustate

--- operazioni.c
+++ operazioni.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main () {
 
-  int n1, n2, som,mol,div,sott;
+  int n1, n2, som,mol,div,sott,rest;
 
   printf ( "dimmi il primo numero :");
   scanf ("%d", &n1);
@@ -12,11 +12,13 @@ int main () {
   sott=n1-n2;
   mol=n1*n2;
   div=n1/n2;
+  rest=n1%n2;
 
   printf ("La somma è %d \n :", som);
   printf ("La sottrazione è %d \n:", sott);
   printf ("La moltiplicazion è %d \n: ", mol);
-  printf ("La divisione è %d \n: " div);
+  printf ("La divisione è %d \n: ", div);
+  printf ("Il resto è %d \n : ", rest)
 
   return 0;
 }
