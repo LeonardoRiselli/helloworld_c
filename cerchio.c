e8d1197f806596ad8b73df5e5791b92a44377179
LeonardoRiselli <juniorriselli@gmail.com>
2017-02-15 13:44:39 +0100

    Cerchio

---
+++ cerchio.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+  double r, sup, cir;
+  
+  printf ("dimmi il raggio ");
+  scanf("%d", &r);
+
+  cir=2*3.14*r;
+  sup=r*r*3.14;
+
+  printf ("la ciconverenza è di %d CM \n", cir);
+  printf ("la superficie è di %d CM \n", sup);
+
+  return 0;
+}
+  
+
+   
