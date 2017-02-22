0dfe5274031a72b14cba9f840d9577d7eda13035
LeonardoRiselli <juniorriselli@gmail.com>
2017-02-22 12:59:29 +0100

    cerchio.c aggiustato

--- cerchio.c
+++ cerchio.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-
-  double r, sup, cir;
+  float r, sup, cir;
   
   printf ("dimmi il raggio ");
-  scanf("%d", &r);
+  scanf("%f", &r);
 
   cir=2*3.14*r;
   sup=r*r*3.14;
-
-  printf ("la ciconverenza è di %d CM \n", cir);
-  printf ("la superficie è di %d CM \n", sup);
+  cir=(int)cir;
+  sup=(int)sup;
+  printf ("la ciconverenza è di %.0f CM \n", cir);
+  printf ("la superficie è di %.0f CM \n", sup);
 
   return 0;
 }
