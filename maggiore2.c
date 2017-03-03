78153835e355081dc6ebb7c0fc0cf2e5ab914377
LeonardoRiselli <juniorriselli@gmail.com>
2017-03-03 10:47:31 +0100

    maggiore2.c aggiustato

--- maggiore2.c
+++ maggiore2.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 int main(){
-  int a,b;
+ int a,b,magg;
   
   printf("dimmi A: ");
   scanf("%d", &a);
   printf("dimmi B: ");
   scanf("%d", &b);
 
-  if (a>b){
-    printf("Il maggiore tra i due è %d \n ",a);
+  magg=a;
+
+  if (b>magg){
+    magg=b;
   }
-  else{
-    printf("Il maggiore tra i due è %d \n ",b);
-  }
+
+  printf("il maggiore tra i due è : %d \n",magg);
 
   return 0;
 }
-  
-  
+
